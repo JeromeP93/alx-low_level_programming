2cf29a50f5162efa81182229cf15a707001afc76
Jerome <jeromespatrick@gmail.com>
2023-06-08 05:43:47 -0700

    Task 7 complete

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,6 @@
 #include "main.h"
+#include <stdbool.h>
 #include <string.h>
-
-/**
- * is_palindrome_helper - Recursive helper function to check
- * if a string is a palindrome.
- * @s: The string to check.
- * @start: The starting index.
- * @end: The ending index.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int is_palindrome_helper(char *s, int start, int end)
-{
-if (start >= end)
-return (1);
-if (s[start] != s[end])
-return (0);
-
-return (is_palindrome_helper(s, start + 1, end - 1));
-}
 
 /**
  * is_palindrome - Checks if a string is a palindrome.
@@ -29,5 +11,13 @@ return (is_palindrome_helper(s, start + 1, end - 1));
 int is_palindrome(char *s)
 {
 int length = strlen(s);
-return (is_palindrome_helper(s, 0, length - 1));
+
+if (length <= 1)
+return (1);
+
+if (s[0] != s[length - 1])
+return (0);
+
+/* Recursive call with the substring excluding the first and last characters */
+return (is_palindrome(s + 1) && is_palindrome(s + length - 1));
 }
