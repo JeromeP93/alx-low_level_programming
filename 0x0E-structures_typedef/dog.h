a151b75154a0bc4e689852f7f8d92edbc14a4495
Jerome <jeromespatrick@gmail.com>
2023-06-19 09:59:34 -0700

    Task 3 complete

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,15 @@
  * @age: The age of the dog
  * @owner: The owner of the dog
  *
- * Description: This structure holds information about a dog, including its
- *              name, age, and owner.
+ * Description: This structure represents a dog with its name, age, and owner.
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
 /* Functions Prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
