6cc10b2522e9ab5dde1446d76c65a4bed38963b8
Jerome <jeromespatrick@gmail.com>
2023-06-22 23:47:25 -0700

    Task 3 fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,83 +3,37 @@
 #include <stdio.h>
 
 /**
- * print_char - Prints a character.
- * @args: The va_list containing the arguments.
- */
-void print_char(va_list args)
-{
-printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - Prints an integer.
- * @args: The va_list containing the arguments.
- */
-void print_int(va_list args)
-{
-printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - Prints a float.
- * @args: The va_list containing the arguments.
- */
-void print_float(va_list args)
-{
-printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - Prints a string.
- * @args: The va_list containing the arguments.
- */
-void print_string(va_list args)
-{
-char *str = va_arg(args, char *);
-
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-}
-
-/**
  * print_all - Prints anything based on the given format.
  * @format: A list of types of arguments passed to the function.
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i = 0, j = 0;
+unsigned int i = 0;
 char *separator = "";
-
-struct print_format
-{
-char format;
-void (*printer)(va_list);
-} valid_formats[] = {
-{'c', print_char},
-{'i', print_int},
-{'f', print_float},
-{'s', print_string},
-{0, NULL}
-};
+char curr_char;
 
 va_start(args, format);
 
 while (format && format[i])
 {
-j = 0;
-while (valid_formats[j].format != '\0')
+curr_char = format[i];
+
+if (curr_char == 'c')
+printf("%s%c", separator, va_arg(args, int));
+else if (curr_char == 'i')
+printf("%s%d", separator, va_arg(args, int));
+else if (curr_char == 'f')
+printf("%s%f", separator, va_arg(args, double));
+else if (curr_char == 's')
 {
-if (valid_formats[j].format == format[i])
-{
-printf("%s", separator);
-valid_formats[j].printer(args);
+char *str_arg = va_arg(args, char *);
+if (str_arg == NULL)
+str_arg = "(nil)";
+printf("%s%s", separator, str_arg);
+}
+
 separator = ", ";
-break;
-}
-j++;
-}
 i++;
 }
 
