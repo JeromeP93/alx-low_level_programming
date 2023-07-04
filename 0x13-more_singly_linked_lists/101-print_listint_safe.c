b9032e7a119d79baad0119757ea262133f0c9da8
JeromeP93 <jeromespatrick@gmail.com>
2023-07-04 18:37:16 +0000

    Task 12 correct output third fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,6 @@
 #include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_listint_safe - Prints a linked list, even if it contains a loop.
@@ -10,44 +10,22 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow, *fast;
+const listint_t *curr = head;
 size_t count = 0;
-int loop_detected = 0;
-slow = head;
-fast = head;
-while (slow && fast && fast->next)
+
+while (curr)
 {
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
+printf("[%p] %d\n", (void *)curr, curr->n);
+count++;
+
+if (curr > curr->next)
 {
-loop_detected = 1;
+printf("-> [%p] %d\n", (void *)curr->next, curr->next->n);
 break;
 }
-count++;
+
+curr = curr->next;
 }
-if (loop_detected)
-{
-slow = head;
-while (slow != fast)
-{
-printf("[%p] %d\n", (void *)slow, slow->n);
-slow = slow->next;
-fast = fast->next;
-count++;
-}
-printf("[%p] %d\n", (void *)slow, slow->n);
-printf("-> [%p] %d\n", (void *)fast, fast->n);
-count++;
-}
-else
-{
-while (head)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-count++;
-}
-}
+
 return (count);
 }
