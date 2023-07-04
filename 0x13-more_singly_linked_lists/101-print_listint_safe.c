483baf311d7eaad25fcc16a7a3d935b6dbf19ac7
JeromeP93 <jeromespatrick@gmail.com>
2023-07-04 18:23:18 +0000

    Task 12 correct output fixed

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint_safe - Prints a linked list, even if it contains a loop.
@@ -12,6 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 const listint_t *slow = head;
 const listint_t *fast = head;
 size_t count = 0;
+int loop_detected = 0;
 
 while (slow && fast && fast->next)
 {
@@ -21,10 +22,26 @@ fast = fast->next->next;
 
 if (slow == fast)
 {
-printf("-> [%p] %d\n", (void *)slow, slow->n);
+loop_detected = 1;
 break;
 }
 count++;
+}
+
+if (loop_detected)
+{
+slow = head;
+while (slow != fast)
+{
+printf("[%p] %d\n", (void *)slow, slow->n);
+slow = slow->next;
+fast = fast->next;
+}
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+}
+else
+{
+printf("No loop found\n");
 }
 
 return (count);
