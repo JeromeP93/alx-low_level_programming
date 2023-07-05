a650cd93e1fb33c10d312b7c34b563f0c208dbfa
JeromeP93 <jeromespatrick@gmail.com>
2023-07-05 06:49:40 +0000

    Task 12 correct output fix second attempt

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,48 @@
-#include "lists.h"
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - Prints a linked list, even if it contains a loop.
- * @head: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: Pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *curr = head;
 size_t count = 0;
+const listint_t *current = head;
+const listint_t *loop_node = NULL;
 
-while (curr)
+while (current)
 {
-printf("[%p] %d\n", (void *)curr, curr->n);
+printf("[%p] %d\n", (void *)current, current->n);
 count++;
+current = current->next;
 
-if (curr > curr->next)
+/* Check for a loop */
+if (current == loop_node)
 {
-printf("-> [%p] %d\n", (void *)curr->next, curr->next->n);
+printf("-> [%p] %d\n", (void *)current, current->n);
 break;
 }
 
-curr = curr->next;
+/* Set loop_node if it's NULL */
+if (loop_node == NULL)
+loop_node = head;
+
+if (loop_node)
+loop_node = loop_node->next;
+
+if (current == NULL || loop_node == NULL)
+continue;
+
+if (current == loop_node)
+{
+printf("-> [%p] %d\n", (void *)current, current->n);
+break;
+}
 }
 
 return (count);
