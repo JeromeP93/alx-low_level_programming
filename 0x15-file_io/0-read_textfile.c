00aadd005f8ac9e4e1ec78c1c3c701e7e769b745
JeromeP93 <jeromespatrick@gmail.com>
2023-07-11 08:00:43 +0000

    Task 0 5th commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it
- * to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the
+ * POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
@@ -12,9 +14,9 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t n_read, n_written;
+int fd, res;
 char buffer[1024];
+ssize_t total_read = 0, total_written = 0;
 
 if (filename == NULL)
 return (0);
@@ -23,21 +25,26 @@ fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-n_read = read(fd, buffer, sizeof(buffer));
-if (n_read == -1)
+while (((res = read(fd, buffer, sizeof(buffer))) > 0)
+&& (total_read < (ssize_t)letters))
+{
+if (total_read + res > (ssize_t)letters)
+res = letters - total_read;
+
+total_written = write(STDOUT_FILENO, buffer, res);
+if (total_written == -1)
 {
 close(fd);
 return (0);
 }
 
-if (n_read > (ssize_t)letters)
-n_read = letters;
+total_read += res;
+}
 
-n_written = write(STDOUT_FILENO, buffer, n_read);
 close(fd);
 
-if (n_written == -1 || n_written != n_read)
+if ((res == -1) || (total_written != total_read))
 return (0);
 
-return (n_written);
+return (total_read);
 }
