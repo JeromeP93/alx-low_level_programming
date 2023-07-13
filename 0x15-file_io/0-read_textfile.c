8478fdf2e092655f271adf651d279af46ff30e73
JeromeP93 <jeromespatrick@gmail.com>
2023-07-13 00:10:56 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to
- * the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * Return: The actual number of letters read and printed. 0 if an error occurs.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, res;
-char buffer[1024];
-ssize_t total_read = 0, total_written = 0;
+int fd;
+ssize_t bytes_read, bytes_written;
+char *buffer;
 
 if (filename == NULL)
 return (0);
@@ -25,27 +23,33 @@ fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-while ((res = read(fd, buffer, sizeof(buffer))) > 0)
-{
-if (total_read + res > (ssize_t)letters)
-res = (ssize_t)letters - total_read;
-
-total_written = write(STDOUT_FILENO, buffer, res);
-if (total_written == -1)
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
 
-total_read += res;
-if (total_read >= (ssize_t)letters)
-break;
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
 }
 
+buffer[bytes_read] = '\0'; /* Null-terminate the buffer */
+
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+free(buffer);
 close(fd);
 
-if (res == -1 || total_written != total_read)
-return (0);
-
-return (total_read);
+return (bytes_written);
 }
