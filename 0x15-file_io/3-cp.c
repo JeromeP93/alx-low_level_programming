6bc1a2ac10f3de077bf14810de2997bc64ba852a
JeromeP93 <jeromespatrick@gmail.com>
2023-07-13 20:04:25 +0000

    Task 3 update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,13 +7,9 @@
 #define BUFFER_SIZE 1024
 
 void display_usage_error(const char *program_name);
-void display_read_error(const char *filename);
-void display_write_error(const char *filename);
-void display_close_error(int fd);
-int open_source_file(const char *filename);
-int open_destination_file(const char *filename);
-void copy_file_content(int source_fd, int destination_fd);
-void close_file(int fd);
+void display_error(const char *message);
+int open_file(const char *filename, int flags, mode_t mode);
+void copy_file(const char *source_filename, const char *destination_filename);
 
 /**
  * main - Entry point. Copies the content of a file to another file.
@@ -24,132 +20,98 @@ void close_file(int fd);
  */
 int main(int argc, char *argv[])
 {
-int source_fd, destination_fd;
-
 if (argc != 3)
 {
 display_usage_error(argv[0]);
-exit(97);
+return (97);
 }
 
-source_fd = open_source_file(argv[1]);
-if (source_fd == -1)
-{
-display_read_error(argv[1]);
-exit(98);
-}
-
-destination_fd = open_destination_file(argv[2]);
-if (destination_fd == -1)
-{
-display_write_error(argv[2]);
-exit(99);
-}
-
-copy_file_content(source_fd, destination_fd);
-
-close_file(source_fd);
-close_file(destination_fd);
+copy_file(argv[1], argv[2]);
 
 return (0);
 }
 
 /**
-* display_usage_error - Displays the usage error message.
-* @program_name: The name of the program.
-*/
+ * display_usage_error - Displays the usage error message.
+ * @program_name: The name of the program.
+ */
 void display_usage_error(const char *program_name)
 {
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", program_name);
 }
 
 /**
-* display_read_error - Displays the read error message.
-* @filename: The name of the file.
-*/
-void display_read_error(const char *filename)
+ * display_error - Displays the error message.
+ * @message: The error message.
+ */
+void display_error(const char *message)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+dprintf(STDERR_FILENO, "Error: %s\n", message);
 }
 
 /**
-* display_write_error - Displays the write error message.
-* @filename: The name of the file.
-*/
-void display_write_error(const char *filename)
+ * open_file - Opens a file with the specified flags and mode.
+ * @filename: The name of the file.
+ * @flags: The flags to use for opening the file.
+ * @mode: The mode to use if creating the file.
+ *
+ * Return: The file descriptor, or -1 on failure.
+ */
+int open_file(const char *filename, int flags, mode_t mode)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-}
-
-/**
-* display_close_error - Displays the close error message.
-* @fd: The file descriptor.
-*/
-void display_close_error(int fd)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-}
-
-/**
-* open_source_file - Opens the source file for reading.
-* @filename: The name of the file.
-*
-* Return: The file descriptor, or -1 on failure.
-*/
-int open_source_file(const char *filename)
-{
-int fd = open(filename, O_RDONLY);
+int fd = open(filename, flags, mode);
+if (fd == -1)
+display_error(filename);
 return (fd);
 }
 
 /**
-* open_destination_file - Opens the destination file for writing.
-* @filename: The name of the file.
-*
-* Return: The file descriptor, or -1 on failure.
-*/
-int open_destination_file(const char *filename)
+ * copy_file - Copies the content from source file to destination file.
+ * @source_filename: The name of the source file.
+ * @destination_filename: The name of the destination file.
+ */
+void copy_file(const char *source_filename, const char *destination_filename)
 {
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-return (fd);
-}
-
-/**
-* copy_file_content - Copies the content from source_fd to destination_fd.
-* @source_fd: The source file descriptor.
-* @destination_fd: The destination file descriptor.
-*/
-void copy_file_content(int source_fd, int destination_fd)
-{
-char buffer[BUFFER_SIZE];
+int source_fd, destination_fd;
 ssize_t bytes_read, bytes_written;
+char buffer[BUFFER_SIZE];
+
+source_fd = open_file(source_filename, O_RDONLY, 0);
+if (source_fd == -1)
+exit(98);
+
+destination_fd = open_file(destination_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (destination_fd == -1)
+{
+close(source_fd);
+exit(99);
+}
 
 while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(destination_fd, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
-display_write_error("destination file");
+display_error(destination_filename);
 exit(99);
 }
 }
 
 if (bytes_read == -1)
 {
-display_read_error("source file");
+display_error(source_filename);
 exit(98);
 }
+
+if (close(source_fd) == -1)
+{
+display_error("Can't close source file");
+exit(100);
 }
 
-/**
-* close_file - Closes the file associated with the file descriptor.
-* @fd: The file descriptor.
-*/
-void close_file(int fd)
+if (close(destination_fd) == -1)
 {
-if (close(fd) == -1)
-{
-display_close_error(fd);
+display_error("Can't close destination file");
 exit(100);
 }
 }
